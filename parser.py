ada68fffbab4ca3a73ded5bf7b42ae1e685a00d3
klepticat <ellie.a.private@gmail.com>
2020-11-20 09:06:38 -0500

    Update parser.py

--- parser.py
+++ parser.py
@@ -11,13 +11,16 @@ while True:
     i = 0
     totalCases = ''
     days = ''
+    dates = ''
 
     # Loop through data of a country
     while i < len(data[country]['data']):
         totalCases += str(data[country]['data'][i].get('total_cases') or '0.0') + ' '
         days += str(i) + ' '
+        dates += str(data[country]['data'][i].get('date') or 'NONE') + ' '
         i += 10
 
     # Print Data
     print('Total Cases:\n' + totalCases)
     print('Days Passed:\n' + days)
+    print('Dates:\n ' + dates)
